f6aab27b85574c9841d8c325ad29b4dde79b7bd6
fieryCaulobacter <jorge.l.flores@mail.mcgill.ca>
2018-08-20 12:40:21 -0400

    Condensed and updated code from signature-norm directory.

--- signature-norm/Normalization_get_avg.py
+++ signature-norm/Normalization_get_avg.py
@@ -1,21 +1,20 @@
 """Created:  Friday July 6, 2018
-   Modified: Wednesday July 11, 2018
+   Modified: Monday August 20, 2018
    Jorge Luis Flores
    Generates a normalizing vector of 940 structural motifs from the collection of all mRNAs
-   This vector is the average of all windows folded and will be used to normalize"""
+   This vector is the average of all windows folded and will be used to normalize."""
 import sys
 sys.path.append('/u/floresj/Pyth_modules/')
 import multiprocessing
-import subprocess
 
 from datetime import datetime as dt
 import time
-from script1_mp import print_time
 
 from Bio.SeqIO import parse
-import motifs_vector_2 as mv
+import motifs_vector_3 as mv
 import numpy as np
 import pandas as pd
+
 
 
 def get_sum_signs(rna, mcff_args='-t 1', mcff_timeout=300, win_size=79, win_skip=1):
@@ -27,12 +26,11 @@ def get_sum_signs(rna, mcff_args='-t 1', mcff_timeout=300, win_size=79, win_skip
        win_size      = length of windows to use
        win_skip      = how many nucleotides to move to the right each time'''
     
-    # aptamer-21 is 79-nucleotides long
     win_count = 0
     signature_vector = np.zeros(940)
     
     for i in range(0, len(rna.seq), win_skip):
-        # exits loop when the passed window is shorter than 79 nt
+        # exits loop when the passed window is shorter than win_size
         if len(rna.seq[i:i+win_size]) < win_size:
             break
         
@@ -51,8 +49,7 @@ if __name__ == '__main__':
         def __init__(self, target):
             self.starttime = time.time()
             self.target = target                # how many TimeCounters must be used
-            self.count = 0                      # how many TimeCounters have been instantiated
-                                                # in this case, this corresponds to the number of transcripts processed
+            self.count = 0                      # number of transcripts processed
             self.vector_sum = np.zeros(940)     # array stores all the results
             self.nb_windows = 0                 # counts how many windows are used
 
@@ -62,16 +59,13 @@ if __name__ == '__main__':
             self.nb_windows += nb_win           # keeps track of number of windows used
             self.count += 1                     # keeps track of number of RNAs processed
             
-            # prints current status
+            # status report
             if self.count % 100 == 0 or self.count == self.target:
                 print(f'{self.count}\tout of\t{self.target} were processed.')
                 
                 ## keep track of execution time
                 with open('/u/floresj/mRNA_norm/normalization_fold_all_log.txt', 'a') as fp:
                     fp.write(f'{dt.now() - start_time}\t{self.count}\n')
-                
-                ## know how much time is left
-                #print_time(self.starttime, self.count, self.total)
     
     # load transcripts from file
     all_mrna = []
@@ -82,15 +76,15 @@ if __name__ == '__main__':
 
     # initialize logging file
     with open('/u/floresj/mRNA_norm/normalization_fold_all_log.txt', 'w') as fp:
-        fp.write('Version: Wednesday July 11, 2018\n')
+        fp.write('Version: Monday August 20, 2018\n')
         fp.write('Time\t\tProcessed\n')
     
     ## keep track of execution time
     start_time = dt.now()
     
     # multiprocessing setup
-    multiprocessing.set_start_method('spawn')
     compteur = TimeCounter(len(all_mrna))
+    multiprocessing.set_start_method('spawn')
     pool = multiprocessing.Pool(36)
 
     for rna in all_mrna:
@@ -103,7 +97,6 @@ if __name__ == '__main__':
     vector_avg = compteur.vector_sum / compteur.nb_windows
     print(f'{compteur.count}\t Sequences folded')
 
-    # save the vector in a file, with the shapes as index
     motif_index = mv.list_of_all_motifs()
 
     vector_df = pd.DataFrame(vector_avg, index=motif_index).transpose()
